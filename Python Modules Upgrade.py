b666138502587fdff61d365e4a8302a235fca2c5
Po-Ming Tsai <34699639+pomingtsai@users.noreply.github.com>
2022-07-05 22:02:34 +0800

    Add files via upload

---
+++ Python Modules Upgrade.py
@@ -0,0 +1,7 @@
+# Python3 Modules Upgrades
+
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+call("pip install --upgrade " + ' '.join(packages), shell=True)
